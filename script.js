289e94fb795df9ea84425509e2facee425fb19a8
FabianaArciniegas <144750464+FabianaArciniegas@users.noreply.github.com>
2023-09-30 15:52:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@
 // Ejericio de referencia con array:
 const nombres = ["camilo", "juan", "laura"]
 nombres.splice(2,0,"maria","jose");
-console.log (nombres);
 document.write(nombres);
+console.log (nombres);
